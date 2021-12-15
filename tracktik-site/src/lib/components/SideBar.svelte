8bcf7864cac573d64d7e3e535f4341ce9f272dd3
bwen <philippe.guilbault@gmail.com>
2021-12-15 16:50:59 -0500

    forgot sidebar comp

---
+++ tracktik-site/src/lib/components/SideBar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import type { Link as LinkType } from '@type/Link.type';
+    import Link from '$components/Link.svelte';
+    import { session } from '$app/stores';
+
+    export let menuItems: LinkType = [];
+
+    menuItems = [
+        {text: 'Dashboards'},
+        {text: 'Sites (Client)'},
+        {text: 'Employees'},
+        {text: 'Maps'},
+        {text: 'Sales & Lists'},
+        {text: 'Settings'},
+    ];
+</script>
+
+<section class="side-bar">
+    <div class="logged-user">
+        <img src="{$session.auth.user.avatar}" />
+    </div>
+    {#each menuItems as item}
+    <div>
+        <Link>{item.text}</Link>
+    </div>
+    {/each}
+</section>
+
+<style lang="css">
+    .side-bar {
+        width: 100px;
+        z-index: 10;
+    }
+
+    .side-bar .logged-user {
+        padding: 16px;
+    }
+</style>
